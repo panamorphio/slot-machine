48a4751c1d6af916f0964481bbf2a23bbebc64a8
Daniil Kähri <daniil@Daniils-MacBook-Air.local>
2025-02-07 13:10:08 +0200

    First commit

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
 
 def deposit():
     while True:

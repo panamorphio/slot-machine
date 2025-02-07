6dec8e4b4c896a9b27bddc50f27d1c6ad1d4fde5
panamorphio <daniil.kahri@gmail.com>
2025-02-07 19:11:50 +0200

    Second commit

--- main.py
+++ main.py
@@ -18,6 +18,30 @@ def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        
+        print()
 
 def deposit():
     while True:
@@ -30,6 +54,7 @@ def deposit():
                 print("Amount must be greater than 0.")
         else:
             print("Please enter a number.")
+
     return amount
 
 def get_number_of_lines():
@@ -43,6 +68,7 @@ def get_number_of_lines():
                 print("Enter a valid number of lines.")
         else:
             print("Please enter a number.")
+
     return lines
 
 def get_bet():
@@ -56,6 +82,7 @@ def get_bet():
                 print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
         else:
             print("Please enter a number.")
+
     return amount
 
 def main():
@@ -69,7 +96,10 @@ def main():
         else:
             break
     
-    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}.")
+    print(
+        f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}.")
 
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
